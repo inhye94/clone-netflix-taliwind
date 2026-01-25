ae62548265b9787b61431b442fd2edde5ed7da1d
inhye94 <inhye.park.dev@gmail.com>
2026-01-25 18:40:39 +0900

    feat: TMDB API 연결

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,5 +1,21 @@
 import { PageLayout } from "../layouts/PageLayout";
 
 export default function ListPage() {
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`,
+    },
+  };
+
+  fetch(
+    "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+    options,
+  )
+    .then((res) => res.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.error(err));
+
   return <PageLayout>ListPage</PageLayout>;
 }
